fc6d764dbfe6f0f71c0a06028163353ce1ec68c8
hilee <eldh2002@gmail.com>
2025-09-23 15:03:22 +0900

    사용제한자 static

---
+++ src/oop/static_/singleton/Main.java
@@ -0,0 +1,22 @@
+package oop.static_.singleton;
+
+import java.util.SimpleTimeZone;
+
+public class Main {
+    public static void main(String[] args) {
+        // new Singleton(); (x)
+        Singleton s1 = Singleton.getInstance();
+        s1.method1();
+        s1.method2();
+
+        // SIngleton.s = null; (x)
+        Singleton s2 = Singleton.getInstance();
+        Singleton s3 = Singleton.getInstance();
+        Singleton s4 = Singleton.getInstance();
+
+        System.out.println("s1 = " + s1);
+        System.out.println("s2 = " + s2);
+        System.out.println("s3 = " + s3);
+        System.out.println("s4 = " + s4);
+    }
+}
---
+++ src/oop/static_/singleton/Singleton.java
@@ -0,0 +1,27 @@
+package oop.static_.singleton;
+
+public class Singleton {
+    // 1. 외부에서 이 클래스의 객체를 생성하지 못하도록
+    // 생성자를 단 하나만 선언하고, 그것을 private로 가려버림
+
+    private Singleton() {
+        System.out.println("객체가 생성됨!");
+    }
+
+    // 2. 생성자를 호출할 수 있는 곳 -> 같은 클래스 내부 뿐.
+    // 스스로의 객체를 단 1개만 생성
+    private static Singleton s = new Singleton();
+
+    // 3. 외부에서 이 클래스의 객체를 요구할 시, 2번에서 미리 만들어 놓은
+    // 단 하나의 객체의 주소값을 공개된 메서드를 통해 리턴한다.
+    public static Singleton getInstance() {
+        return s;
+    }
+    public void method1() {
+        System.out.println("여러 클래스에서 사용하는 중요한 메서드");
+    }
+
+    public void method2() {
+        System.out.println("프로그램 내에서 최소 한번씩은 호출해야 하는 필수 메서드!");
+    }
+}
