6e88531539e021d7d2fdac6eb10233a65db96112
hilee <eldh2002@gmail.com>
2025-09-24 11:15:39 +0900

    객체지향 5대 개념 SOLID - SRP

---
+++ src/oop/solid/srp/PayCalculator.java
@@ -0,0 +1,11 @@
+package oop.solid.srp;
+
+// 급여 관련 로직에만 집중
+public class PayCalculator {
+    // 책임 1: 직원 월급 계산
+    public long calculatePay(Employee e) {
+        // 복잡한 급여 계산 로직이 여기 존재함
+        // 세금 계산, 보너스 계산, 연차-반차 체크.....
+        return e.getBaseSalary();
+    }
+}
