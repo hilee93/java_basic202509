a5908e7e7f4fe7841747e5fdf8697da8e9ef655a
hilee <eldh2002@gmail.com>
2025-09-24 11:15:21 +0900

    객체지향 5대 개념 SOLID - OCP

---
+++ src/oop/solid/ocp/DiscountCalculator.java
@@ -0,0 +1,8 @@
+package oop.solid.ocp;
+
+public class DiscountCalculator {
+    public long calculateDiscount(DiscountPolicy policy, long price) {
+        // 계산기는 그저 전달받은 정책에 따라 계산만 합니다.
+        return policy.getDiscountPrice(price);
+    }
+}
---
+++ src/oop/solid/ocp/GoldDiscountPolicy.java
@@ -0,0 +1,8 @@
+package oop.solid.ocp;
+
+public class GoldDiscountPolicy implements DiscountPolicy {
+    @Override
+    public long getDiscountPrice(long price) {
+        return price * 10 / 100;
+    }
+}
