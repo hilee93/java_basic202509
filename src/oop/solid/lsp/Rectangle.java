e9f4f19b06dfbd2b3cd5979fa5933e8ac1f36cac
hilee <eldh2002@gmail.com>
2025-09-24 11:33:15 +0900

    객체지향 5대 개념 SOLID - LSP

---
+++ src/oop/solid/lsp/Rectangle.java
@@ -0,0 +1,25 @@
+package oop.solid.lsp;
+
+// 부모 클래스
+public class Rectangle implements Shape {
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @Override
+    public int getArea() {
+        return width * height;
+    }
+}
---
+++ src/oop/solid/lsp/Square.java
@@ -0,0 +1,13 @@
+package oop.solid.lsp;
+
+public class Square implements Shape {
+    private int sideLength; // 정사각형은 한 변의 길이만 있으면 ok!
+
+    public Square(int sideLength) {
+        this.sideLength = sideLength;
+    }
+    @Override
+    public int getArea() {
+        return sideLength * sideLength;
+    }
+}
